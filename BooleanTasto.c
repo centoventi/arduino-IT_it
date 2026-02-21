78da928122d42980b54e8b65e1cde9e7fabbcd11
Centoventi_ <78879655+centoventi@users.noreply.github.com>
2026-02-22 00:42:58 +0100

    Re-upload di BooleanTasto
    Design della logica fixato.

--- BooleanTasto.c
+++ BooleanTasto.c
@@ -1,18 +1,19 @@
 bool TASTO;
-void setup() {
+void setup() 
+{
   pinMode(13, OUTPUT); // Configura il pin 13 come output
   pinMode(3, INPUT);   // Configura il pin 3 come input
 }
-void loop() {
+void loop() 
+{
   TASTO = digitalRead(3);
   if (TASTO == HIGH) 
   {
     digitalWrite(13, HIGH); // Accendo il LED
     delay(8000);             // Delay di 800 ms
-  } else
+  } 
+  else
   
-  
-    
-    digitalWrite(13, LOW);  // Spengo il LED
+  digitalWrite(13, LOW);  // Spengo il LED
   
 }
